4555a706045aa660df7eb6f60e14dd998748e33b
Karl Augustsson <karl.augustsson@gmail.com>
2015-09-24 15:03:02 +0200

    first commit

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+require 'json'
+class SearchController < ApplicationController
+	layout "music"
+  def index
+  	@search = open("http://localhost:3000/v1/search_music_groups?search=cranberries").read
+
+  	
+
+
+
+
+  end
+end
