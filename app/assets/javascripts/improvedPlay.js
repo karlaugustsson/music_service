0ac0408fd72cc8857852e3d3a19b2552b2a8221e
Karl Augustsson <karl.augustsson@gmail.com>
2015-09-25 22:55:21 +0200

    messy

--- app/assets/javascripts/improvedPlay.js
+++ app/assets/javascripts/improvedPlay.js
@@ -1,12 +1,14 @@
 function play(id){
-
+var playButton = document.getElementsByName('playbutton')
 var audio = document.getElementById('myplayer');
-console.log(id)
+
 if(!id){
 	id = 0
 }
 
-insertMeta(audio,queuesonglist[id])
+var playButton = document.getElementsByName('playbutton')
+playButton[0].id = "playing"
+insertMeta(audio,id)
 songPlaying = id
 audio.load()
 audio.play()
@@ -18,20 +20,63 @@ var audio = document.getElementById('myplayer')
 audio.pause()
 }
 function nextTrack(){
+if(queuesonglist.length == 0){
 
-nextID = getSongPlaying() + 1
-console.log(queuesonglist[nextId] )
-if(null == queuesonglist[nextId]){
-	play(0)
-	pause()
+	return false
 }
 
-	play(nextID)
+if(isItShuffleTime()){
+	
+	var nextID = randomTrack()
 
 
+}else{
+var nextID = getSongPlaying() + 1
+
+}
+
+if(!queuesonglist[nextID]){
+	console.log(0)
+	play(0)
+	pause()
+}else{
+		play(nextID)
+}
+
+
+
+}
+
+function resumePlayback(audio){
+	audio.play()
 }
 
 function prevTrack(){
+
+if(queuesonglist.length == 0){
+
+	return false
+}
+if(isItShuffleTime()){
+	
+	
+		var prevID = playedTracks[playedTracks.length - 2]
+		console.log(prevID)
+
+
+
+}else{
+var prevID = getSongPlaying() - 1
+
+}
+
+if(!queuesonglist[prevID]){
+	console.log(0)
+	play(0)
+	pause()
+}else{
+		play(prevID)
+}
 
 }
 
@@ -49,40 +94,96 @@ function setVolume(){
 
 
 function getSongPlaying(){
-return songPlaying
+return parseInt(songPlaying)
 }
 
 function isPlaying(audio){
 	{ return !audio.paused; }
 }
-function insertMeta(audio,object){
+function insertMeta(audio,id){
 
 
-	var artist = object["album"]["music_groups"][0]["name"]
+	var artist = queuesonglist[id]["album"]["music_groups"][0]["name"]
 
-	var track = object["music_file_file_name"]
+	var track = queuesonglist[id]["music_file_file_name"]
 	
-	var type = object["music_file_content_type"]
+	var type = queuesonglist[id]["music_file_content_type"]
 
 	data = '<source src="' + track + '"'  + ' type="' + type + '" name ="source" />' ; 
 
 	audio.innerHTML = data
 }
-function newPlayQueue(id){
-	queuesonglist = [];
-	for (var i = id ; songlist[i]; i++) {
-		queuesonglist.push(songlist[i])
-	};
+function newPlayQueue(){
+
+		queuesonglist = songlist
+
+
 
 }
 function updatePlayListAndPlay(){
-	newPlayQueue(this.id)
-	play()
+	newPlayQueue()
+	play(this.id)
 }
 
-function findNextsong(source){
+function playPause(){
+var audio = document.getElementById('myplayer');
+if (this.id == 'notplaying'){
+
+	setTimeout(function(){
+
+
+			resumePlayback(audio)
+
+
+	}, 200);
+	this.id = "playing"
+
+}else{
+	setTimeout(function(){
+		pause()
+	}, 200);
+
+	this.id = "notplaying"
+}
+
+}
+
+function toggleShuffle(){
+	if(this.className == "shuffle off"){
+		this.className = "shuffle on"
 	
 
+	}else{
+		this.className = "shuffle off"
+
+	}
+}
+function isItShuffleTime(){
+	var shuffleButton = document.getElementById('shuffle');
+	return shuffleButton.className == "shuffle on"
+}
+function randomTrack(){
+while(true){
+
+	var number = Math.floor(Math.random() * queuesonglist.length -1) + 1
+		if (playedTracks.indexOf(number) > -1 == false){
+			playedTracks.push(number)
+
+			break;
+		}else if(queuesonglist.length == playedTracks.length){
+			playedTracks = []
+			number = queuesonglist.length + 1
+			break;
+		}
 
 }
+return number
+
+}
+
+function shuffleIsSet(){
+	return shuffleSet
+}
+
+
 
