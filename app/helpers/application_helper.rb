816e7aef1957155f63cf8752e54ab2d7d8a8fd5c
Karl Augustsson <karl.augustsson@gmail.com>
2015-11-17 22:25:17 +0100

    the end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 		render(:partial => "application/error_messages" , :locals => {:object => object})
 	end
 	def check_if_touch()
-		 if request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPad|iPod|Silk|Opera Mini|BlackBerry|AppleWebKit|Android)/]
+		 if request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPad|iPod|Silk|Opera Mini|BlackBerry|Android)/]
 		 	return true
 		 else
 		 	return false
